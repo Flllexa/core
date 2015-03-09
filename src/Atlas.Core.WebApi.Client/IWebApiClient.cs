185690d2fa4addc08a9308317ca5e085eb428c82
acraven <andrew@epworth-consulting.co.uk>
2015-03-09 18:13:26 +0000

    Add empty project

---
+++ src/Atlas.Core.WebApi.Client/IWebApiClient.cs
@@ -0,0 +1,11 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWebApiClient.cs" company="Epworth Consulting Ltd.">
+//   © Epworth Consulting Ltd.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Atlas.Core.WebApi.Client
+{
+   public interface IWebApiClient
+   {
+   }
+}
---
+++ src/Atlas.Core.WebApi.Client/Implementations/WebApiClient.cs
@@ -0,0 +1,11 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WebApiClient.cs" company="Epworth Consulting Ltd.">
+//   © Epworth Consulting Ltd.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Atlas.Core.WebApi.Client.Implementations
+{
+   public class WebApiClient : IWebApiClient
+   {
+   }
+}
