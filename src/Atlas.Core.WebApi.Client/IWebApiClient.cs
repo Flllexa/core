c1b66e3503ec63057cbc32906cefdf8d7d33c23f
acraven <andrew@epworth-consulting.co.uk>
2015-03-09 19:58:11 +0000

    Add config to build nuget package

--- src/Atlas.Core.WebApi.Client/IWebApiClient.cs
+++ src/Atlas.Core.WebApi.Client/IWebApiClient.cs
@@ -7,5 +7,6 @@ namespace Atlas.Core.WebApi.Client
 {
    public interface IWebApiClient
    {
+      TResponse Post<TRequest, TResponse>(string baseUrl, string api, TRequest request);
    }
 }
--- src/Atlas.Core.WebApi.Client/Implementations/WebApiClient.cs
+++ src/Atlas.Core.WebApi.Client/Implementations/WebApiClient.cs
@@ -7,5 +7,9 @@ namespace Atlas.Core.WebApi.Client.Implementations
 {
    public class WebApiClient : IWebApiClient
    {
+      public TResponse Post<TRequest, TResponse>(string baseUrl, string api, TRequest request)
+      {
+         throw new System.NotImplementedException();
+      }
    }
 }
