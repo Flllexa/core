60fbf45bb82d2010344bb945e639f77982fd6842
acraven <andrew@epworth-consulting.co.uk>
2015-03-09 21:14:34 +0000

    Add webapiclient implementation

--- src/Atlas.Core.WebApi.Client/Implementations/WebApiClient.cs
+++ src/Atlas.Core.WebApi.Client/Implementations/WebApiClient.cs
@@ -5,11 +5,51 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace Atlas.Core.WebApi.Client.Implementations
 {
+   using Newtonsoft.Json;
+
+   using RestSharp;
+   using RestSharp.Serializers;
+
    public class WebApiClient : IWebApiClient
    {
       public TResponse Post<TRequest, TResponse>(string baseUrl, string api, TRequest request)
       {
-         throw new System.NotImplementedException();
+         var restClient = new RestClient(baseUrl);
+         var restRequest = new RestRequest(api, Method.POST)
+            {
+               RequestFormat = DataFormat.Json,
+               JsonSerializer = new JsonSerialiser()
+            };
+
+         restRequest.AddBody(request);
+
+         var restResponse = restClient.Execute(restRequest);
+
+         // TODO: Create serialisation abstraction if the serialisation needs customisation
+         var response = JsonConvert.DeserializeObject<TResponse>(restResponse.Content);
+         return response;
+      }
+
+      private class JsonSerialiser : ISerializer
+      {
+         public JsonSerialiser()
+         {
+            this.ContentType = "application/json";
+         }
+
+         public string RootElement { get; set; }
+
+         public string Namespace { get; set; }
+
+         public string DateFormat { get; set; }
+
+         public string ContentType { get; set; }
+
+         public string Serialize(object obj)
+         {
+            // TODO: Create serialisation abstraction if the serialisation needs customisation
+            return JsonConvert.SerializeObject(obj);
+         }
       }
    }
 }
