143d63ce2114665a2aabe5a88bb171fb246ec1bc
acraven <andrew@epworth-consulting.co.uk>
2015-03-22 10:31:29 +0000

    Add console logger implementation

---
+++ src/Atlas.Core.Logging.Tests/ConsoleLoggerShould.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConsoleLoggerShould.cs" company="Epworth Consulting Ltd.">
+//   © Epworth Consulting Ltd.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Atlas.Core.Logging.Tests
+{
+   using System;
+
+   using Atlas.Core.Logging;
+
+   using NUnit.Framework;
+
+   public class ConsoleLoggerShould
+   {
+      private ConsoleLogger componentUnderTest;
+
+      [SetUp]
+      public void SetupBeforeEachTest()
+      {
+         this.componentUnderTest = new ConsoleLogger();
+      }
+
+      [Test]
+      public void NotThrowExceptionWhenLogErrorCalled()
+      {
+         Assert.That(() => this.componentUnderTest.LogError("LogError", new Exception()), Throws.Nothing);
+      }
+
+      [Test]
+      public void NotThrowExceptionWhenLogInfoCalled()
+      {
+         Assert.That(() => this.componentUnderTest.LogInfo("LogInfo"), Throws.Nothing);
+      }
+
+      [Test]
+      public void NotThrowExceptionWhenLogWarningCalled()
+      {
+         Assert.That(() => this.componentUnderTest.LogWarning("LogWarning"), Throws.Nothing);
+      }
+
+      [Test]
+      public void NotThrowExceptionWhenLogDebugCalled()
+      {
+         Assert.That(() => this.componentUnderTest.LogDebug("LogDebug"), Throws.Nothing);
+      }
+   }
+}
---
+++ src/Atlas.Core.Logging/ConsoleLogger.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConsoleLogger.cs" company="Epworth Consulting Ltd.">
+//   © Epworth Consulting Ltd.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Atlas.Core.Logging
+{
+   using System;
+
+   public class ConsoleLogger : ILogger
+   {
+      public void LogError(string format, Exception exception, params object[] args)
+      {
+         var message = string.Format(format, args);
+         Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss.fff} ERROR {1}", DateTime.Now, message);
+         Console.WriteLine(exception.ToString());
+      }
+
+      public void LogWarning(string format, params object[] args)
+      {
+         var message = string.Format(format, args);
+         Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss.fff} WARN  {1}", DateTime.Now, message);
+      }
+
+      public void LogInfo(string format, params object[] args)
+      {
+         var message = string.Format(format, args);
+         Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss.fff} INFO  {1}", DateTime.Now, message);
+      }
+
+      public void LogDebug(string format, params object[] args)
+      {
+         var message = string.Format(format, args);
+         Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss.fff} DEBUG {1}", DateTime.Now, message);
+      }
+   }
+}
