51559111c78f6771ff143186c06949204a97ef29
acraven <andrew@epworth-consulting.co.uk>
2015-04-07 11:41:47 +0100

    Add guid extension methods

---
+++ src/Atlas.Core/GuidExtensions.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GuidExtensions.cs" company="Epworth Consulting Ltd.">
+//   © Epworth Consulting Ltd.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Atlas.Core
+{
+   using System;
+   using System.Collections.Generic;
+
+   public static class GuidExtensions
+   {
+      public static string ToToken(this Guid guid)
+      {
+         var value = guid.ToString().ToUpper().Replace("-", null);
+
+         return string.Join("-", SplitString(value));
+      }
+
+      private static IEnumerable<string> SplitString(string value)
+      {
+         var i = 0;
+
+         while (i < value.Length)
+         {
+            var split = value.Substring(i, 4);
+
+            yield return split;
+
+            i += split.Length;
+         }
+      }
+   }
+}
