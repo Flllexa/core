62a574cba6595e4c5108b493198fc1a8fd3f2df3
acraven <andrew@epworth-consulting.co.uk>
2015-04-07 12:00:05 +0100

    Add application path

---
+++ src/Atlas.Core/IO/DefaultApplicationPath.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefaultApplicationPath.cs" company="Epworth Consulting Ltd.">
+//   © Epworth Consulting Ltd.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Atlas.Core.IO
+{
+   using System;
+
+   public class DefaultApplicationPath : IApplicationPath
+   {
+      public string Path
+      {
+         get { return AppDomain.CurrentDomain.BaseDirectory; }
+      }
+
+      public string ConfigPath
+      {
+         get { return AppDomain.CurrentDomain.BaseDirectory; }
+      }
+   }
+}
---
+++ src/Atlas.Core/IO/IApplicationPath.cs
@@ -0,0 +1,14 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IApplicationPath.cs" company="Epworth Consulting Ltd.">
+//   © Epworth Consulting Ltd.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Atlas.Core.IO
+{
+   public interface IApplicationPath
+   {
+      string Path { get; }
+
+      string ConfigPath { get; }
+   }
+}
